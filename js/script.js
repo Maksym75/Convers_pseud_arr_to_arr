23c33632f6320f46d3c12a7b24ff7c4f6e0ac569
MaksymKolii <maksymkolii@gmail.com>
2024-01-27 18:49:21 +0100

    deleted console.logs

--- js/script.js
+++ js/script.js
@@ -105,5 +105,3 @@ wrapper.append(div)
 //div.innerHTML = 'Hello World'
 div.innerHTML = '<h1>Hello World</h1>'
 // div.textContent = '<h1>Hello World</h1>'
-div.insertAdjacentHTML('beforebegin', '<p>h. yykkb</p>')
-console.log('ojhoihh8op9y')
